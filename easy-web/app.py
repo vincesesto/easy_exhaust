699274535816d122510aa0020fab23f407280818
Vince Sesto <vincesesto@Vinces-MacBook-Pro.local>
2019-08-08 17:03:00 +1200

    Updating with more details

--- easy-web/app.py
+++ easy-web/app.py
@@ -28,9 +28,7 @@ def index():
         "create_date": ""
     }
  
-    context = {
-        "welcome_page": welcome_page,
-    }
+    context = {"welcome_page": welcome_page}
 
     template = render("chalicelib/templates/index.html", context)
     return Response(template, status_code=200, headers={
@@ -38,5 +36,47 @@ def index():
         "Access-Control-Allow-Origin": "*"
     })
 
+@app.route('/signup')
+def sign_up():
+    context = {"signup_page": "test"}
 
+    template = render("chalicelib/templates/sign_up.html", context)
+    return Response(template, status_code=200, headers={
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*"
+    })
+
+@app.route('/strava_auth')
+def strava_auth():           
+    # Redirect to strava auth with steem_name
+    # capture details 
+   
+    resp = app.current_request.to_dict()
+    body = app.current_request.json_body
+    # Add it into the database for tokens
+    steem_name=resp['query_params']['steem_name']
+    
+    strava_auth_url="http://www.strava.com/oauth/authorize?client_id=31940&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=profile:write,activity:write,activity:read_all&steem_name={}".format(steem_name)
+
+    # Now return a response and redirect to new page
+    context = {'strava_auth_url': strava_auth_url}
+    template = render("chalicelib/templates/strava_auth_redirect.html", context)
+    return Response(template, status_code=200, headers={
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*"
+    })
+
+
+#Then get this
+#THIS IS THE CODE YOU WILL KEEP FOR THE USER TO GET BEARER TOKENS
+#http://localhost/exchange_token?state=&code=c4e6eb4d70a1ff0845ff64661e6948e29c8f1003&scope=read,activity:write,profile:write
+
+# Use something like this to get all the details from the retured url
+#@app.route('/exchange_token')
+#def exchange():
+#    # return dictionary of results
+#    resp = app.current_request.to_dict()
+#    body = app.current_request.json_body
+#    # Add it into the database for tokens
+#    get_app_db().add_item(token=resp['query_params']['code'])
 

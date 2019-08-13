d778f79b732f8698c084012ef34237c121b9608c
Vince Sesto <vincesesto@Vinces-MacBook-Pro.local>
2019-08-13 15:57:38 +1200

    Getting the complete process to work

--- easy-web/app.py
+++ easy-web/app.py
@@ -1,5 +1,6 @@
 import os
 import jinja2
+import requests
 from datetime import datetime
 from chalice import Chalice, Response
 from chalicelib import db
@@ -9,6 +10,10 @@ app = Chalice(app_name='easy-web')
 app.debug = True
 _DB = None
 DEFAULT_USERNAME = 'default'
+api_token = '6769ae72996e05a36ac38d546871dd63982b2651'
+client_id = 31940
+athlete_url_base = 'https://www.strava.com/api/v3/athlete'
+strava_auth_url = 'https://www.strava.com/oauth/token'
 
 def get_athlete_db():
     global _DB
@@ -19,6 +24,30 @@ def get_athlete_db():
 def render(tpl_path, context):
     path, filename = os.path.split(tpl_path)
     return jinja2.Environment(loader=jinja2.FileSystemLoader(path or "./")).get_template(filename).render(context)
+
+def update_activity_bearer(user_key):
+    # Update their latest_bearer for the user so you can check more activity
+    # Update this in the database
+    try:
+       response = requests.post(strava_auth_url,
+                            params={'client_id': client_id, 'client_secret': api_token, 'code': user_key,
+                            'grant_type': 'authorization_code'})
+       access_info = dict()
+       activity_data = response.json()
+       access_info['access_token'] = activity_data['access_token']
+       return access_info
+    except:
+        print("Log - An Error occurred trying to authenticate with the {} Strava token".format(user_key))
+        return False
+
+def check_new_user(activity_bearer):
+        # Search through users strava details
+        # Verify it is not in db already
+        bearer_header = "Bearer " + str(activity_bearer)
+        headers = {'Content-Type': 'application/json', 'Authorization': bearer_header}
+        response = requests.get( athlete_url_base, headers=headers )
+        data = response.json()
+        return data
 
 @app.route('/athletes', methods=['GET'])
 def get_athletes():
@@ -77,11 +106,6 @@ def strava_auth():
         "Access-Control-Allow-Origin": "*"
     })
 
-
-#Then get this
-#THIS IS THE CODE YOU WILL KEEP FOR THE USER TO GET BEARER TOKENS
-#http://localhost/exchange_token?state=&code=c4e6eb4d70a1ff0845ff64661e6948e29c8f1003&scope=read,activity:write,profile:write
-
 # Use something like this to get all the details from the retured url
 @app.route('/exchange_token')
 def exchange():
@@ -103,6 +127,31 @@ def all_finished():
     resp = app.current_request.to_dict()
     strava_token=resp['query_params']['strava_token']
     steem_name=resp['query_params']['steem_name']
+
+    # Get the user activity bearer from strava
+    activity_bearer = update_activity_bearer(strava_token)
+
+    print(activity_bearer)
+
+    # Get extra details from strava
+    values = check_new_user(activity_bearer['access_token'])
+
+    print(values)
+    
+    # post details to the database
+
+    new_user = {"athlete": values['id'], "strava_token": strava_token, "activity_bearer": activity_bearer, "steemit_user": steem_name, "steemit_token": None,  "metadata": {}}
+
+    print(new_user)
+
+    get_athlete_db().add_athlete(
+        athlete=values['id'],
+        strava_token=strava_token,
+        activity_bearer=activity_bearer['access_token'],
+        steemit_user=steem_name,
+        steemit_token=None,
+        metadata={},
+    )
     
     context = {'strava_token': strava_token,
                'steem_name': steem_name }
